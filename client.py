c61c0f801edd3915c5c38c16e923164fc1331b3c
fox8090 <matthew-fox@hotmail.co.uk>
2021-02-14 13:00:53 +0000

    Add error handling.

--- client.py
+++ client.py
@@ -1,22 +1,47 @@
+import sys
 import socket
 import threading
+import time
+
+'''python server.py [port]python client.py [username] [hostname] [port'''
 
 def start_connection():
     clientSocket = socket.socket()
-    clientSocket.connect(('localhost', 8800))
+    try:
+        clientSocket.connect((hostname, int(port)))
+    except:
+        print("Server is not available or port/hostname are wrong.")
+        exit()
+
+    threading.Thread(target=receiver, args=(clientSocket, )).start()
+    threading.Thread(target=sender, args=(clientSocket, )).start()
     
-    threading._start_new_thread(start_listening, (clientSocket, 1))
-    #threading._start_new_thread(send_to_all, (clientSocket, 1))
 
-def start_listening(clientSocket, q):
+def sender(clientSocket):
+    clientSocket.send(("type:n data:" + username).encode())
     while True:
-        message = clientSocket.recv(1024).decode()
-        print(message)
+        message = input()
+        if message == 'quit':
+            clientSocket.close()
+            break
+        clientSocket.send(("type:a data:"+message).encode())
+        
 
-def send_to_all(clientSocket, q):
+def receiver(clientSocket):
     while True:
-        message = input(">> ")
-        clientSocket.send(message.encode())
+        try:
+            received = clientSocket.recv(1024)
+            if received:
+                print("\r" + received.decode() + "\n>> ", end="")
+        except:
+            exit()
 
 if __name__ == "__main__":
-    start_connection()
+    if len(sys.argv) != 4:
+        print("Invalid arguments.\nPlease use the following format: python client.py [username] [hostname] [port]\n")
+        exit()
+    else:
+        username = sys.argv[1]
+        hostname = sys.argv[2]
+        port = sys.argv[3]
+        start_connection()
--- server.py
+++ server.py
@@ -1,34 +1,75 @@
+import sys
 import socket
 import threading
 
 def start_server():
     serverSocket = socket.socket()
-    serverSocket.bind(('', 8800))
-    serverSocket.listen()
+    serverSocket.bind(('', int(port)))
+    serverSocket.listen(1)
     print("SERVER IS RECEIVING")
     while True:
         connection, clientAddress = serverSocket.accept()
-        print("join from ", clientAddress)
-        welcomeMessage = "welcome enter name"
-        connection.send(welcomeMessage.encode())
+        print("[SERVER] New connection from", clientAddress)
         allClients[connection] = clientAddress
-        threading._start_new_thread(new_connection, (connection, clientAddress))
+        threading._start_new_thread(new_connection, (connection,))
 
-def new_connection(connection, clientAddress):
-    user = connection.recv(1024).decode()
-    helpInfo = "Welcome " + user + ", heres how to use"
+def new_connection(connection):
+    user = None
+    while user is None:
+        try:
+            message = connection.recv(1024).decode()
+            if message[5] == 'n':
+                user = message[12:]
+                allNames[connection] = user
+            else:
+                user = None
+        except:
+            connection.send("[SERVER] A valid username is required.")
+    helpInfo = "[SERVER] Welcome " + user + "! Type 'quit' to exit. Enjoy!"
     connection.send(helpInfo.encode())
-    message = user + "has joined"
+    message = "[SERVER] " + user + " has joined"
     send_to_all(message)
+    print(message)
     while True:
-        message = connection.recv(1024).decode()
-        send_to_all(message)
-        print("{}: {}".format(clientAddress, message))
+        try:
+            message = connection.recv(1024).decode()
+            if message:
+                print(message)
+                if message[5] == 'a':
+                    print("here")
+                    out = "[{}] {}".format(allNames[connection], message[12:])
+                    send_to_all(out)
+                    print(out)
+                else:
+                    print("ERROR")
+        except:
+            print("[SERVER] Lost connection from", allClients[connection])
+            del allClients[connection]
+            out = "[SERVER] " + allNames[connection] + " has left"
+            send_to_all(out)
+            print(out)
+            del allNames[connection]
+            break
 
 def send_to_all(message):
     for client in allClients:
-        client.send(message.encode())
+        try:
+            client.send(message.encode())
+        except:
+            closeAll()
+
+def closeAll():
+    for client in allClients:
+        client.close()
+    print("ERROR")
+    exit()
 
 if __name__ == "__main__":
     allClients = {}
+    allNames ={}
+    if len(sys.argv) != 2:
+        print("Invalid arguments.\nPlease use the following format: python server.py [port]")
+        exit()
+    else:
+        port = sys.argv[1]
     start_server()
